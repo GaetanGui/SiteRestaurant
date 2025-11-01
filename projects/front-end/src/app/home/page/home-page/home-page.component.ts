f02cc2b45b5554c765ca8e89e5b5144ab7f1966b
GaetanGui <41259628+GaetanGui@users.noreply.github.com>
2025-11-02 00:55:31 +0100

    update mobile version

--- projects/front-end/src/app/home/page/home-page/home-page.component.ts
+++ projects/front-end/src/app/home/page/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { GjtButtonComponent } from '../../../common/components/gjt-button/gjt-button.component';
 import { RouterLink } from '@angular/router';
+import { ResponsiveService } from '../../../common/services/responsive.service';
 
 @Component({
   selector: 'tgam-home-page',
@@ -15,5 +16,6 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-
+  constructor(public responsive: ResponsiveService) {
+  }
 }
